faad3e05244ac017be1c850a51936627431772bd
Sujeet <sujeet.22010762@viit.ac.in>
2024-02-11 04:05:45 +0530

    Added module-info

--- src/alu.v
+++ src/alu.v
@@ -1,12 +1,19 @@
 /*
-# Author:		Sujeet Jagtap
-# Module:		alu
-# Description:	
-# Inputs:		
-# Outputs:		
+# Author:			Sujeet Jagtap
+# Module:			alu
+# Description:		Arithmetic Logic Unit module of the CPU
+# Input Ports:		SrcA			: Source for A
+						SrcB			: Source for B
+						ALUControl	: Control Signal for ALU
+# Output Ports:	ALUResult	: Result from the ALU
+						zero			: zero signal (high if ALUResult is zero)
 */
 module alu (
-	
+	input [31:0] SrcA,
+	input [31:0] SrcB,
+	input [2:0] ALUControl,
+	output zero,
+	output [31:0] ALUResult
 );
 
 
